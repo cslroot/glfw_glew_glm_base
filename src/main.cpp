06fe75f366a30398365bfd9cae92b1c28d80914b
rootx <cslroot@gmail.com>
2021-07-09 08:31:13 +0900

    OpenGL4.1 core profile sample

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <iostream>
+#include <vector>
 static const struct
 {
     float x, y;
@@ -51,11 +52,11 @@ static const struct
         {0.f, 0.6f, 0.f, 0.f, 1.f}};
 
 static const char *vertex_shader_text =
-    "#version 110\n"
+    "#version 410 core\n"
     "uniform mat4 MVP;\n"
-    "attribute vec3 vCol;\n"
-    "attribute vec2 vPos;\n"
-    "varying vec3 color;\n"
+    "in vec3 vCol;\n"
+    "in vec2 vPos;\n"
+    "out vec3 color;\n"
     "void main()\n"
     "{\n"
     "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
@@ -63,11 +64,12 @@ static const char *vertex_shader_text =
     "}\n";
 
 static const char *fragment_shader_text =
-    "#version 110\n"
-    "varying vec3 color;\n"
+    "#version 410 core\n"
+    "in vec3 color;\n"
+    "out vec4 fragColor;\n"
     "void main()\n"
     "{\n"
-    "    gl_FragColor = vec4(color, 1.0);\n"
+    "    fragColor = vec4(color, 1.0);\n"
     "}\n";
 
 static void error_callback(int error, const char *description)
@@ -92,8 +94,10 @@ int main(void)
     if (!glfwInit())
         exit(EXIT_FAILURE);
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
     if (!window)
@@ -109,11 +113,15 @@ int main(void)
     // gladLoadGL(glfwGetProcAddress);
     glfwSwapInterval(1);
 
-    // NOTE: OpenGL error checks have been omitted for brevity
+    // print opengl context version
+    printf("GL version: %s\n", glGetString(GL_VERSION));
+    printf("GL shading language version: %s\n",
+           glGetString(GL_SHADING_LANGUAGE_VERSION));
+    auto title = "GL ver.: " + std::string((char *)glGetString(GL_VERSION));
+    title += "  /  GLSL ver.: " + std::string((char *)glGetString(GL_SHADING_LANGUAGE_VERSION));
+    glfwSetWindowTitle(window, title.c_str());
 
-    glGenBuffers(1, &vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // NOTE: OpenGL error checks have been omitted for brevity
 
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
@@ -132,12 +140,23 @@ int main(void)
     vpos_location = glGetAttribLocation(program, "vPos");
     vcol_location = glGetAttribLocation(program, "vCol");
 
-    glEnableVertexAttribArray(vpos_location);
-    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void *)0);
-    glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void *)(sizeof(float) * 2));
+    // for 4.1 use VAO instead of VBO
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    {
+        glGenBuffers(1, &vertex_buffer);
+        glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glEnableVertexAttribArray(vpos_location);
+        glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+                              sizeof(vertices[0]), (void *)0);
+
+        glEnableVertexAttribArray(vcol_location);
+        glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+                              sizeof(vertices[0]), (void *)(sizeof(float) * 2));
+    }
 
     while (!glfwWindowShouldClose(window))
     {
